ef166984bd30738acf571456e0bb4457b200dccb
Melissa Stadt <mstadt@uwaterloo.ca>
2021-11-03 15:21:59 -0400

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,24 @@
+"""
+A simple example for a git workflow
+"""
+
+# imports
+
+
+# define the rhs of system of odes
+
+def main():
+
+    # define the rates dictionary
+
+    # define the parameter dictionary (pick whatever values you like!)
+
+    # Solve the system
+
+    # plot the system and save the figure
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
