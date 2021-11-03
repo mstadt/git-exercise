b45945c2e749fa70f0a27e9f2efb2cdc09eca7d5
Lena P <35874882+lpodina@users.noreply.github.com>
2021-11-03 15:40:45 -0400

    started parameter dict

--- demo.py
+++ demo.py
@@ -10,6 +10,9 @@ A simple example for a git workflow
 def main():
 
     # define the rates dictionary
+    rates_dict = {}
+    rates_dict["rho"] = 5.
+    rates_dict["k"] = 10.
 
     # define the parameter dictionary (pick whatever values you like!)
 
